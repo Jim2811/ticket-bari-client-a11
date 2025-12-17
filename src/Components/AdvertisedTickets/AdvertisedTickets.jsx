95752a12f8718ade540d652eeb4bf5d11bfb2e19
Rukhsat <rhjim2005@gmail.com>
2025-12-18 01:27:29 +0600

    Fetched data using tanstack query & axios and fixed advertise section

--- src/Components/AdvertisedTickets/AdvertisedTickets.jsx
+++ src/Components/AdvertisedTickets/AdvertisedTickets.jsx
@@ -1,30 +1,16 @@
 import React from "react";
-
+import useAxios from "../../Hooks/useAxios";
+import { useQuery } from "@tanstack/react-query";
+import AdvertisedTicketsCard from "./AdvertisedTicketsCard";
 const AdvertisedTickets = () => {
-  const advertisedTickets = [
-    {
-      id: 1,
-      route: "Dhaka → Rangpur",
-      price: 550,
-      image: "/bus.png",
-      type: "Bus",
+  const axiosInstance = useAxios();
+  const { data: advertisedTickets } = useQuery({
+    queryKey: ["advertised"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/tickets/advertised");
+      return res.data;
     },
-    {
-      id: 2,
-      route: "Dhaka → Chittagong",
-      price: 700,
-      image: "/train.png",
-      type: "Train",
-    },
-    {
-      id: 3,
-      route: "Dhaka → Sylhet",
-      price: 1200,
-      image: "/plane.png",
-      type: "Flight",
-    },
-  ];
-
+  });
   return (
     <>
       <section className="py-10 bg-accent">
@@ -32,27 +18,7 @@ const AdvertisedTickets = () => {
           Admins Choice
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-6">
-          {advertisedTickets.map((ticket) => (
-            <div key={ticket.id} className="card bg-white shadow-md">
-              <figure>
-                <img
-                  src={ticket.image}
-                  alt={ticket.route}
-                  className="h-48 w-full object-cover"
-                />
-              </figure>
-              <div className="card-body">
-                <h3 className="text-xl font-semibold">{ticket.route}</h3>
-                <p className="text-gray-600">Type: {ticket.type}</p>
-                <p className="text-[#16A34A] font-bold">৳ {ticket.price}</p>
-                <div className="card-actions justify-end">
-                  <button className="btn bg-primary text-white hover:bg-[#15803D]">
-                    Book Now
-                  </button>
-                </div>
-              </div>
-            </div>
-          ))}
+          {advertisedTickets.map((ticket) => <AdvertisedTicketsCard key={ticket?._id} ticket= {ticket}></AdvertisedTicketsCard>)}
         </div>
       </section>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,14 @@ import "./App.css";
 import "./index.css";
 import route from "./Router/Routes.jsx";
 import AuthProvider from "./Provider/AuthProvider.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={route}></RouterProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={route}></RouterProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
